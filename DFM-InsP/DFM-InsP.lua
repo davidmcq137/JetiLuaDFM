78a7ddc9a9ff3720259bb7e1089c55447f4802fa
David McQueeney <davidmcq@us.ibm.com>
2022-12-19 22:53:19 -0500

    misc

--- DFM-InsP/DFM-InsP.lua
+++ DFM-InsP/DFM-InsP.lua
@@ -19,6 +19,16 @@ local hSlider = {
    {-6,6}
 }
 
+local messages = { "Turbine status: Ready",
+		   "Turbine status: Starting",
+		   "Turbine status: Running",
+		   "Turbine status: Idle",
+		   "Turbine status: Cooling",
+		   "Turbine status: Off"}
+
+msgidx = 1
+nextTime =0
+
 local function readSensors(tt)
    local sensorLbl = "***"
    local sensors = system.getSensors()
@@ -39,18 +49,24 @@ local function prefix()
    return pf
 end
 
+local function drawTextCenter(x, y, str, font)
+   if not font then font = FONT_NORMAL end
+   lcd.drawText(x - lcd.getTextWidth(font, str)/2,
+		y - lcd.getTextHeight(font)/2, str, font)
+end
+
 local function drawShape(col, row, shape, f, rotation)
 
    local sinShape, cosShape
    local ren = lcd.renderer()
-
+   local fw = f^0.7
    sinShape = math.sin(rotation)
    cosShape = math.cos(rotation)
    ren:reset()
    for _, point in pairs(shape) do
       ren:addPoint(
-	 col + (f*point[1] * cosShape - f*point[2] * sinShape + 0.5),
-	 row + (f*point[1] * sinShape + f*point[2] * cosShape + 0.5)
+	 col + (fw*point[1] * cosShape - f*point[2] * sinShape + 0.5),
+	 row + (fw*point[1] * sinShape + f*point[2] * cosShape + 0.5)
       ) 
    end
    ren:renderPolygon()
@@ -62,9 +78,55 @@ end
 
 local function printForm()
 
-   --if cfimg then lcd.drawImage(0, 0, cfimg) end
-   --if gimg then lcd.drawImage(0, 0, gimg) end
+   if InsP.backImg  then lcd.drawImage(0, 0, InsP.backImg) end
+   if InsP.panelImg then lcd.drawImage(0, 0, InsP.panelImg) end
 
+   local ctlr = system.getInputs("P5")
+   local ctl = (ctlr+1)/2
+   local rot = -0.75*math.pi * (1-ctl) + 0.75*math.pi*(ctl)
+   local factor = 1.0
+
+   
+   ip = InsP.panels[1]
+   for i, widget in ipairs(ip) do
+      --print(widget.type)
+
+      if widget.type == "roundGauge" then
+	 lcd.setColor(255,255,255)
+	 factor = widget.radius / 65.0
+	 	 drawShape(widget.x0, widget.y0, needle_poly_large, factor, rot + math.pi)
+
+      elseif widget.type == "horizontalBar" then
+
+	 lcd.setColor(0,0,0)
+	 --lcd.setColor(200,200,200)
+	 local hPad = widget.height / 4
+	 local vPad = widget.height / 8
+	 local start = widget.x0 - widget.width / 2 + hPad
+	 local w = math.floor(widget.width - 2 * hPad + 0.5)
+	 local h = math.floor(widget.height - 2 * vPad + 0.5)
+	 local cellMult = 0.4
+	 local cellOff = math.floor((1-cellMult) / 2 * h + 0.5)
+	 lcd.drawFilledRectangle(start + ctl*(w+1)+1*(1-ctl), -1+widget.y0 - h/2 + cellOff,
+				 math.floor((1-ctl)*(w+1)+0.5), math.floor(h*cellMult+0.5)+2)
+	 lcd.setColor(255,255,255)
+	 drawTextCenter(widget.x0, widget.y0+h/2-hPad/5, "Fuel Remaining", FONT_MINI)
+      elseif widget.type == "textBox" then
+
+	 lcd.setColor(0,0,0)
+	 if system.getTimeCounter() > nextTime then
+	    msgidx = msgidx + 1
+	    if msgidx > #messages then msgidx = 1 end
+	    nextTime = system.getTimeCounter() + 1000*2
+	 end
+	 
+	 local str = messages[msgidx]
+	 lcd.drawText(widget.x0 - lcd.getTextWidth(FONT_BOLD, str)/2,
+		      widget.y0 - lcd.getTextHeight(FONT_BOLD)/2, str, FONT_BOLD)
+	 
+      else
+      end
+   end
 end
 
 local function init()
@@ -92,16 +154,18 @@ local function init()
 
    table.sort(InsP.panels)
 
-   -- FOR TESTING  .. hardwire one test file
+   -- FOR TESTING  .. hardwire one test file .. json and image
    
-   fn = pDir .. "/test" .. ".jsn"
+   fn = pDir .. "/panel320.jsn"
    local file = io.readall(fn)
    InsP.panels[1] = json.decode(file)
-      
+   InsP.panelImg = lcd.loadImage(pDir .. "/panel320.png")
+   InsP.backImg = lcd.loadImage(pDir .. "/cfimage.png")
+
    readSensors(InsP)
 
    system.registerForm(1, MENU_APPS, "Instrument Panel", initForm, keyForm)
-   system.registerTelemetry(1, "test window", 4, printForm)
+   system.registerTelemetry(1, "Instrument Panel", 4, printForm)
 
 end
 
