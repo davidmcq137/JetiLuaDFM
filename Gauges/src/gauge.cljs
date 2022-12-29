1fd27ae086e77f3503794f51adc37116d87c0115
Russell McQueeney <rmcq@utexas.edu>
2022-12-28 16:08:02 -0800

    forms for round gauge

--- Gauges/src/gauge.cljs
+++ Gauges/src/gauge.cljs
@@ -8,8 +8,11 @@
 
 (def db (atom {}))
 
+(def draw-scale 2)
+(def disp-scale 2)
+
 (defn shape->bbox
-  [{:strs [type radius x0 y0 width height]}]
+  [{:strs [type radius x0 y0 width height] :as sh}]
   (case type
     "roundGauge" (let [d (* 2 radius)]
                    [(- x0 radius) (- y0 radius) d d])
@@ -19,21 +22,23 @@
           halfh (* 0.5 height)]
       [(- x0 halfw) (- y0 halfh) width height])
     
-    (println "Do not understand shape of" (pr-str type))))
+    (println "Do not understand shape of" (pr-str type) "SH" sh)))
 
 (rum/defc bitmap-canvas-drag
-  [bmap ix iy k scl]
+  [bmap ix iy k]
   (let [cref (rum/create-ref)
         [{:keys [x y mousedown] :as drag-state} set-drag-state!] (rum/use-state {})
         pos-top (str (or y iy) "px")
         pos-left (str (or x ix) "px")
         scaled-x (js/Math.round
-                  (+ (/ (or x ix) scl)
-                     (* 0.5 (.-width bmap))))
+                  (/ (+ (/ (or x ix) draw-scale)
+                        (* 0.5 (.-width bmap)))
+                     disp-scale))
         
         scaled-y (js/Math.round
-                  (+ (/ (or y iy) scl)
-                     (* 0.5 (.-height bmap))))]
+                  (/ (+ (/ (or y iy) draw-scale)
+                        (* 0.5 (.-height bmap)))
+                     disp-scale))]
     
     (rum/use-effect!
      (fn []
@@ -56,10 +61,10 @@
                :height (.-height bmap)
                :style {:position :absolute
                        ;; :background-color (if (:mousedown drag-state) "tomato" "peachpuff")
-                       :width (* scl (.-width bmap))
-                       :height (* scl (.-height bmap))
+                       :width (* disp-scale (.-width bmap))
+                       :height (* disp-scale (.-height bmap))
                        ;; :outline "1px solid blue"
-                       :border (if-not mousedown "none" "1px solid #fff")
+                       :outline (if-not mousedown "none" "1px solid #fff")
                        :top pos-top
                        :left pos-left
                        :z-index (if mousedown 999 0)
@@ -84,7 +89,7 @@
                                             (.-clientY ev))]
                                   (set-drag-state! (assoc drag-state
                                                           :x (- (.-offsetLeft (.-target ev))
-                                                                dx)
+                                                                 dx)
                                                           :y (- (.-offsetTop (.-target ev))
                                                                 dy)
                                                           :xinit (.-clientX ev)
@@ -101,24 +106,19 @@
 
 
 (defn render-gauge*
-  [i]
-  (let [[x y w h :as bbox] (shape->bbox i)
-        c (js/OffscreenCanvas. w h)
-        ctx (doto (.getContext c "2d")
-              (.translate (- x) (- y)))]
+  ([i] (render-gauge* i draw-scale))
+  ([i scl]
+   (let [[x y w h :as bbox] (shape->bbox i)
+         c (js/OffscreenCanvas. (* w scl) (* h scl))
+         ctx (doto (.getContext c "2d")
+               (.scale scl scl)
+               (.translate (- x) (- y)))]
 
-    (js/renderGauge ctx (clj->js i))
+      (js/renderGauge ctx (clj->js i))
     
-    { ;; :bbox bbox
-     :bitmap (.transferToImageBitmap c)
-     :params i}))
-
-(defn render-gauges!
-  []
-  (reset! db
-          {:gauges (zipmap (range)
-                           (map render-gauge* static-input-data))}))
-
+     { ;; :bbox bbox
+      :bitmap (.transferToImageBitmap c)
+      :params i})))
 
 (rum/defc static-bitmap-canvas
   [bmap]
@@ -158,6 +158,137 @@
      (when-let [err (:error state)]
        [:span.json-error err])]))
 
+
+(rum/defc gaugeparam-slider < rum/reactive
+  [da k props]
+  (let [{:keys [params] :as d} (rum/react da)
+        v (get params k)]
+    [:input (merge
+             {:type "range"
+              :min 0
+              :max 100
+              :value v
+              :onChange (fn [^js ev]
+                          (swap! da
+                                 (fn [g]
+                                   (render-gauge*
+                                    (assoc (:params g)
+                                           k
+                                           (js/parseFloat (.-value (.-target ev))))))))}
+             props)]))
+
+(rum/defc gaugeparam-text < rum/reactive
+  [da k]
+  (let [{:keys [params] :as d} (rum/react da)
+        v (get params k)]
+    [:input {:type "text"
+             :value v
+             :onChange (fn [^js ev]
+                         (swap! da
+                                (fn [g]
+                                  (render-gauge*
+                                   (assoc (:params g) k (.-value (.-target ev)))))))}]))
+
+(rum/defc gaugeparam-plusminus < rum/reactive
+  [da k]
+  (let [{:keys [params] :as d} (rum/react da)
+        v (get-in params k)]
+    [:span.plusminus {}
+     [:input {:type "button"
+              :value "-"
+              :onClick #(swap! da (fn [g] (render-gauge* (update-in (:params g) k dec))))}]
+     [:input {:type "text"
+              :style {:margin "0 1ex 0 1ex"}
+              :value v
+              :onChange (fn [^js ev]
+                          (swap! da
+                                 (fn [g]
+                                   (render-gauge*
+                                    (assoc-in (:params g)
+                                              k
+                                              (js/parseInt (.-value (.-target ev))))))))}]
+     [:input {:type "button"
+              :value "+"
+              :onClick #(swap! da (fn [g] (render-gauge* (update-in (:params g) k inc))))}]]))
+
+(rum/defc edit-colorvals < rum/reactive
+  [da]
+  (let [{:keys [params]} (rum/react da)
+        cvs (get params "colorvals")
+        n (count cvs)]
+    [:div.colorvals {}
+     (for [i (range (count cvs))
+           :let [{:strs [val color]} (nth cvs i)
+                 minv (if (= i 0)
+                        (get params "min")
+                        (get (nth cvs (dec i)) "val"))
+                 maxv (if (= i (dec n))
+                        (get params "max")
+                        (get (nth cvs (inc i)) "val"))]
+           t [:label :range :slider]]
+       (case t
+         :range [:span {:key (str "l" i)} (str val)]
+          
+         :label [:input {:type "text"
+                         :key (str "c" i)
+                         :value color
+                         :onChange (fn [^js ev]
+                                     (swap! da
+                                            (fn [g]
+                                              (render-gauge*
+                                               (assoc-in (:params g)
+                                                         ["colorvals" i "color"]
+                                                         (.-value (.-target ev)))))))}]
+         :slider
+         [:input {:type "range"
+                  :key (str "s" i)
+                  :min minv
+                  :max maxv
+                  :value val
+                  :onChange (fn [^js ev]
+                              (swap! da
+                                     (fn [g]
+                                       (render-gauge*
+                                        (assoc-in (:params g)
+                                                  ["colorvals" i "val"]
+                                                  (js/parseInt (.-value (.-target ev))))))))}]))
+     [:input.add-arc
+      {:type "button"
+       :value "+"
+       :onClick #(swap! da (fn [g] (render-gauge* (update (:params g)
+                                                          "colorvals"
+                                                          conj {"color" "red"
+                                                                "val" (get params "max")}))))}]]))
+
+(rum/defc edit-roundgauge < rum/reactive
+  [da]
+  (let [{:keys [params] :as d} (rum/react da)]
+    (rum/fragment
+     
+     [:span.slider-label "Label"]
+     (gaugeparam-text da "label")
+     
+     [:span.slider-label "Minimum"]
+     (gaugeparam-plusminus da ["min"])
+     
+     [:span.slider-label "Maximum"]
+     (gaugeparam-plusminus da ["max"])
+     
+     [:span.slider-label "Value"]
+     (gaugeparam-slider da "value" {:min (get params "min")
+                                    :max (get params "max")})
+     
+     
+     
+     [:span.slider-label "Divisions"]
+     (gaugeparam-plusminus da ["divs"])
+     
+     [:span.slider-label "Subdivisions"]
+     (gaugeparam-plusminus da ["subdivs"])
+     
+     "Colors"
+     (edit-colorvals da))))
+
 (rum/defc onegauge-editor < rum/reactive
   [da]
   (let [d (rum/react da)
@@ -167,22 +298,27 @@
      (static-bitmap-canvas (:bitmap d))
     
      [:div.sliders
-      [:span.slider-label (str "X=" x0)]
+      [:span.slider-label (str "X = " x0)]
       [:input {:type "range" :min 0 :max 320
                :value x0
                :onChange (fn [^js ev]
                            (swap! da assoc-in [:params "x0"]
                                   (.-value (.-target ev))))}]
-      [:span.slider-label (str "Y=" y0)]
+      [:span.slider-label (str "Y = " y0)]
       [:input {:type "range" :min 0 :max 320
                :value y0
                :onChange (fn [^js ev]
                            (swap! da assoc-in [:params "y0"]
                                   (.-value (.-target ev))))}]
+      
+      (case (get (:params d) "type")
+        "roundGauge" (edit-roundgauge da)
+        nil)
+      
       (when-not (:jsonedit d)
-          [:input {:type "button"
-                :value "Edit json"
-                :onClick (fn [] (swap! da assoc :jsonedit true))}])]
+        [:input {:type "button"
+                 :value "Edit json"
+                 :onClick (fn [] (swap! da assoc :jsonedit true))}])]
    
      (when (:jsonedit d)
        (rum/fragment
@@ -205,7 +341,6 @@
   (let [cref (rum/create-ref)
         w 320
         h 160
-        scl 4
         gauges (:gauges (rum/react db))]
     [:div.container
      [:ul.inputs
@@ -214,8 +349,8 @@
           i))]
      
      [:div.composite
-      {:style {:width (str (* w scl) "px")
-               :height (str (* h scl) "px")
+      {:style {:width (str (* w draw-scale disp-scale) "px")
+               :height (str (* h draw-scale disp-scale) "px")
                :position :relative}}
 
       (for [[i d] gauges
@@ -223,9 +358,10 @@
             :let [[x y w h] (shape->bbox (:params d))]]
         (->> i
              (rum/with-key (bitmap-canvas-drag
-                            (:bitmap d) (* x scl) (* y scl)
-                            [:gauges i]
-                            scl))))]
+                            (:bitmap d)
+                            (* x draw-scale disp-scale)
+                            (* y draw-scale disp-scale)
+                            [:gauges i]))))]
      [:ul 
       
       [:input {:type "button"
@@ -257,13 +393,17 @@
                                    (fn [v] (ask-download-file "gauges.png" v)))))
                }]]]))
 
+(defn reload-json!
+  []
+  (-> (js/fetch "/gauges.json")
+      (.then (fn [fr] (.json fr)))
+      (.then (fn [jd]
+               (reset! db
+                       {:gauges (zipmap (range)
+                                        (map render-gauge* (js->clj jd)))})))))
+
 (defn  ^:dev/after-load init []
   (let [el (.getElementById js/document "root")]
-    (-> (js/fetch "/gauges.json")
-        (.then (fn [fr] (.json fr)))
-        (.then (fn [jd]
-                 (reset! db
-                         {:gauges (zipmap (range)
-                                          (map render-gauge* (js->clj jd)))}))))
+    (reload-json!)
 
     (rum/mount (root) el)))
