1726e265af079d652bf36549944ccb48f6da2290
David McQueeney <davidmcq@us.ibm.com>
2023-05-01 09:29:08 -0400

    misc

--- Gauges/resources/gauges/pandoc.sh
+++ Gauges/resources/gauges/pandoc.sh
@@ -14,5 +14,6 @@ UNAME=$(echo "$BNAME" | awk '{print toupper($0)}')
 HNAME='/home/davidmcq/JS/DFM-InsP/Panels/EN-'$UNAME'.HTML'
 echo 'Processing' $1 
 jq -r '."doc-md"' $FNAME | sed 's/\\n/\n/g' > 'en-'$BNAME'.md'
-pandoc 'en-'$BNAME'.md' -f markdown_strict -t html -o $HNAME --metadata title=$BNAME --template="jeti.html"
+pandoc 'en-'$BNAME'.md' -t html -o $HNAME --from markdown_strict-smart --metadata title=$BNAME --template="jeti.html"
+sed -i s/[”“]/'"'/g $HNAME
 cp $HNAME '/home/davidmcq/JSE/DOCS/DFM-INSP/'
